60a265cf89d686053ae2074fd2f32abaf9ae7b59
chadillejones <chadille.jones@bison.howard.edu>
2019-11-28 14:32:49 -0500

    subprogram A

--- project3.s
+++ project3.s
@@ -97,7 +97,7 @@ processString:   #subprogram A to accept all the string and make it substrings
 	skip_trailing_tab_or_space: #checks if the rest of the substring tabs and spaces
 		addi $t3, $t3, 1 #move to the next byte
 		lb $t5, 0($t3)  #gets a character of the string
-		beq $t5, $t1, valid_input #branches if only trailing tabs are spaces are found before newline
+		beq $t5, $t1, validSubstring #branches if only trailing tabs are spaces are found before newline
 		bne $t4, $t8, not_a_space #branches if character is not a space
 		j skip_trailing_tab_or_space #returns to check next character for trailing tab or space
 	

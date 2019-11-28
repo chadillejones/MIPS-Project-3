5416b55ea0d20eb3064e176d16dc38ffc38f5450
chadillejones <chadille.jones@bison.howard.edu>
2019-11-28 01:50:44 -0500

    Initializing variables

--- project3.s
+++ project3.s
@@ -1,2 +1,9 @@
 #Chadille Jones
 #02856918 % 11 = 9 Base 35
+
+.data #section to declare data
+user_input: .space 2000 # creating space for the user input
+invalid: .asciiz "NaN" #creating the error message for incorrect input
+
+.text #Assembly language instruction
+.globl main
